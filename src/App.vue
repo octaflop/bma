434728fab5bd385cdecbd7b4131615a724523506
Faris Chebib <faris.chebib@teem.com>
2017-10-04 13:22:31 -0600

    Messin around

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          1 of 4
+        </b-col>
+        <b-col>
+          2 of 4
+        </b-col>
+        <b-col>
+          3 of 4
+        </b-col>
+        <b-col>
+          4 of 4
+        </b-col>
+      </b-row>
+    </b-container>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
